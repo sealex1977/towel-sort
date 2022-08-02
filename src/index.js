0843615e1afada58d4f1b3dc7151ede5fbcfe7fc
Oleksandr Serebriakov <sealex1977@gmail.com>
2022-08-03 00:59:48 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+  let myArray = [];
+
+  if (Array.isArray(matrix) && matrix.length > 0) {
+
+    for (i = 0; i < matrix.length; i++) {
+
+      if (i % 2 !== 0) {
+        matrix[i] = matrix[i].reverse();
+      }
+
+      let elements = matrix[i];
+
+      for (n = 0; n < elements.length; n++) {
+        myArray.push(elements[n]);
+      }
+    }
+  }
+
+  return myArray;
 }
+
+// module.exports = function towelSort (matrix) {
+//   return matrix = (matrix === undefined) ? [] : matrix.reduce((prev, item, index) => index % 2 === 0 ? prev.concat(item) : prev.concat(item.reverse()), []);
+// }
+
+/*module.exports = function towelSort (matrix) {
+  if (matrix === undefined) return [];
+  return arr = matrix.reduce((prev, item, index) => index % 2 === 0 ? prev.concat(item) : prev.concat(item.reverse()), []);
+}*/
+
+/*module.exports = function towelSort (matrix) {
+  let arr = [];
+  if (matrix === undefined) return arr;
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      arr = arr.concat(matrix[i]);
+    } else {
+      arr = arr.concat(matrix[i].reverse());
+    }
+  }
+  return arr;
+}*/
